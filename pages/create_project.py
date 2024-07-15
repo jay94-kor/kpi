730a975e126c3ddddb191a490394b7fcd953db8b
jay94-kor <148678404+jay94-kor@users.noreply.github.com>
2024-07-15 23:09:56 +0900

    ,

--- pages/create_project.py
+++ pages/create_project.py
@@ -14,35 +14,36 @@ roles = []
 total_percentage = 0
 
 for i in range(5):
-    with st.expander(f'Role {i + 1}'):
-        role_name = st.text_input(f'역할 이름 {i + 1}', key=f'role_name_{i}')
-        responsibility = st.text_area(f'책임 {i + 1}', key=f'responsibility_{i}')
-        percentage = st.number_input(f'비중 (%) {i + 1}', min_value=0.0, max_value=100.0, key=f'percentage_{i}')
-        total_percentage += percentage
+    st.subheader(f'Role {i + 1}')
+    role_name = st.text_input(f'역할 이름 {i + 1}', key=f'role_name_{i}')
+    responsibility = st.text_area(f'책임 {i + 1}', key=f'responsibility_{i}')
+    percentage = st.number_input(f'비중 (%) {i + 1}', min_value=0.0, max_value=100.0, key=f'percentage_{i}')
+    total_percentage += percentage
+    
+    tasks = []
+    task_total_percentage = 0
+    st.write(f'Tasks for Role {i + 1}')
+    for j in range(5):
+        st.write(f'Task {j + 1}')
+        task_name = st.text_input(f'테스크 이름 {j + 1}', key=f'task_name_{i}_{j}')
+        task_percentage = st.number_input(f'테스크 비중 (%) {j + 1}', min_value=0.0, max_value=100.0, key=f'task_percentage_{i}_{j}')
+        task_total_percentage += task_percentage
         
-        tasks = []
-        task_total_percentage = 0
-        for j in range(5):
-            with st.expander(f'Task {j + 1} for Role {i + 1}'):
-                task_name = st.text_input(f'테스크 이름 {j + 1}', key=f'task_name_{i}_{j}')
-                task_percentage = st.number_input(f'테스크 비중 (%) {j + 1}', min_value=0.0, max_value=100.0, key=f'task_percentage_{i}_{j}')
-                task_total_percentage += task_percentage
-                
-                employees = []
-                employee_total_percentage = 0
-                employee_list = get_employees()
-                for k in range(len(employee_list)):
-                    employee_name = st.text_input(f'직원 이름 {k + 1}', value=employee_list[k].name, key=f'employee_name_{i}_{j}_{k}', disabled=True)
-                    employee_percentage = st.number_input(f'투입률 (%) {k + 1}', min_value=0.0, max_value=100.0, key=f'employee_percentage_{i}_{j}_{k}')
-                    employee_total_percentage += employee_percentage
-                    if employee_percentage > 0:
-                        employees.append({'name': employee_name, 'percentage': employee_percentage, 'id': employee_list[k].id})
-                
-                if task_name and task_percentage > 0 and employee_total_percentage == 100:
-                    tasks.append({'name': task_name, 'percentage': task_percentage, 'employees': employees})
+        employees = []
+        employee_total_percentage = 0
+        employee_list = get_employees()
+        for k in range(len(employee_list)):
+            employee_name = st.text_input(f'직원 이름 {k + 1}', value=employee_list[k].name, key=f'employee_name_{i}_{j}_{k}', disabled=True)
+            employee_percentage = st.number_input(f'투입률 (%) {k + 1}', min_value=0.0, max_value=100.0, key=f'employee_percentage_{i}_{j}_{k}')
+            employee_total_percentage += employee_percentage
+            if employee_percentage > 0:
+                employees.append({'name': employee_name, 'percentage': employee_percentage, 'id': employee_list[k].id})
+        
+        if task_name and task_percentage > 0 and employee_total_percentage == 100:
+            tasks.append({'name': task_name, 'percentage': task_percentage, 'employees': employees})
 
-        if role_name and responsibility and percentage > 0 and task_total_percentage == 100:
-            roles.append({'name': role_name, 'responsibility': responsibility, 'percentage': percentage, 'tasks': tasks})
+    if role_name and responsibility and percentage > 0 and task_total_percentage == 100:
+        roles.append({'name': role_name, 'responsibility': responsibility, 'percentage': percentage, 'tasks': tasks})
 
 if st.button('생성'):
     if total_percentage == 100:
